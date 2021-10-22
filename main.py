32f7667084d037d169f900a6371f649658f4be98
Samhuw8a <Samuel.huwiler@gmx.ch>
2021-10-22 14:35:21 +0200

    Bischen Umschreiben

--- main.py
+++ main.py
@@ -5,30 +5,23 @@ import sys
 def main():
 
     #setup
+    #  Prüfung von argv
+    hiden = True if "-h" in sys.argv else False
+    is_v2 = True if "--v2" in sys.argv else False
 
-    #Prüfen ob --v2 angegeben wird
-    if "-h" in sys.argv:
-        hiden=True
-    else: hiden=False
-
-    if "--v2" in sys.argv:
-        is_v2=True
-    else: is_v2=False
-
-    #Instanzieren der front und back_end Klassen
+    #  Instanzieren der front und back_end Klassen
     render_eng = Render(is_v2,hiden)
     handler=Eventhandler(render_eng,"Escaperoom Samuel")
-    
-    #ladet die frames aus der json datei in das backend
+    #  ladet die frames aus der json datei in das backend
     handler.impjson("Test.json")
     handler.loadframe(0)
 
     #Haupt schliefe
     while True:
-        #wenn ein nächster frame existiert dann wird dieser geladen
+        #  wenn ein nächster frame existiert dann wird dieser geladen
         if not handler.loadnextframe(): break
     
-    #Endet das game mit dem Endscreen
+    #  Endet das game mit dem Endscreen
     handler.end_game()
 
 if __name__ == '__main__':
